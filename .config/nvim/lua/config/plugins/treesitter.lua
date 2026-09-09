7a9ba1f097ba626fe2e9956080d21ff69cc56751
Matthew <matthew.hambrecht@patapsco.ai>
2026-09-09 12:47:22 -0400

    fix: Remap annoying ass tmux paste bind

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,40 +1,63 @@
+local ensure_installed = {
+  "lua",
+  "vim",
+  "vimdoc",
+  "python",
+  "javascript",
+  "typescript",
+  "tsx",
+  "svelte",
+  "html",
+  "css",
+  "graphql",
+  "http", -- kulala
+  "prisma",
+  "rust",
+  "go",
+  "c",
+  "cpp",
+  "bash",
+  "json",
+  "yaml",
+  "toml",
+  "markdown",
+  "markdown_inline",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- main branch requires nvim 0.12 nightly, master works on 0.11
-  branch = "master",
+  -- main branch requires nvim 0.12
+  branch = "main",
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "python",
-        "javascript",
-        "typescript",
-        "tsx",
-        "svelte",
-        "html",
-        "css",
-        "graphql",
-        "http", -- kulala
-        "prisma",
-        "rust",
-        "go",
-        "c",
-        "cpp",
-        "bash",
-        "json",
-        "yaml",
-        "toml",
-        "markdown",
-        "markdown_inline",
-      },
-      -- install missing parsers when entering a buffer
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+    local ts = require("nvim-treesitter")
+    ts.install(ensure_installed)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("treesitter_start", {}),
+      callback = function(args)
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if not lang then
+          return
+        end
+
+        local function start()
+          vim.treesitter.start(args.buf, lang)
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+
+        if vim.treesitter.language.add(lang) then
+          start()
+        elseif vim.tbl_contains(ts.get_available(), lang) then
+          -- install missing parsers when entering a buffer
+          ts.install(lang):await(function()
+            if vim.api.nvim_buf_is_valid(args.buf) then
+              start()
+            end
+          end)
+        end
+      end,
     })
   end,
 }
